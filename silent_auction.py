7ff4211e12f554010cac593dc32d2b9443873371
sailesh chikkam <sailesh.ch118@gmail.com>
2025-11-04 21:09:23 +0530

    Implement silent auction functionality

---
+++ silent_auction.py
@@ -0,0 +1,33 @@
+import os
+logo = ('''
+╔═╗┬┬  ┌─┐┌┐┌┌┬┐  ╔═╗┬ ┬┌─┐┌┬┐┬┌─┐┌┐┌  ╔═╗┬─┐┌─┐┌─┐┬─┐┌─┐┌┬┐  
+╚═╗││  ├┤ │││ │   ╠═╣│ ││   │ ││ ││││  ╠═╝├┬┘│ ││ ┬├┬┘├─┤│││  
+╚═╝┴┴─┘└─┘┘└┘ ┴   ╩ ╩└─┘└─┘ ┴ ┴└─┘┘└┘  ╩  ┴└─└─┘└─┘┴└─┴ ┴┴ ┴  
+''')
+
+def find_winner(bidder_details):  # sailesh:10000,ram:44545
+    highest_bid = 0
+    winner = ""
+    for bidder in bidder_details:  # sailesh
+        bidding_price = bidder_details[bidder]
+        if bidding_price > highest_bid:
+            highest_bid = bidding_price
+            winner = bidder
+    print(f"Here is the list of all the bidders:{bidder_details}")
+    print(f"the winner is {winner} with a bid price of {highest_bid}")
+
+
+bidder_data = {}
+end_of_bidding = False
+while not end_of_bidding:
+    print(logo)
+    name = input("What is your name? : ")
+    price = int(input("What is your bid amount? : "))
+    bidder_data[name] = price
+    more_bidders = input("Are there more bidders? Type 'yes' or 'no':\n").lower()
+
+    if more_bidders == 'no':
+        end_of_bidding = True
+        find_winner(bidder_data)
+    elif more_bidders == 'yes':
+        os.system('cls')
